22c0ba62f50c792ee76ecb013519dc0fa2c533a8
Justina <justina.desriute@record-evolution.de>
2018-07-31 13:43:23 +0200

    commiting working files in the repository

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import './App.css'
+import WorldMap from './WorldMap'
+import worlddata from './worlddata.js'
+import { range, sum } from 'd3-array'
+import { scaleThreshold } from 'd3-scale'
+import { geoCentroid } from 'd3-geo'
+
+const appdata = worlddata.features.filter(d => geoCentroid(d)[0] < -20)
+
+appdata
+    .forEach((d, i) => {
+        const offset = Math.random()
+      d.launchday = i
+        d.data = range(30).map((p, q) =>
+            q < i ? 0 : Math.random() * 2 + offset)
+    })
+
+class App extends Component {
+  render() {
+
+    const colorScale = scaleThreshold().domain([5, 10, 20, 30])
+      range(["#75739F", "#5EAFC6", "#41A368", "#93C464"])
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>mapzzz</h2>
+        </div>
+        <div>
+          <WorldMap colorScale={colorScale} data={appdata} size={[500, 400]} />
+        </div>
+        </div>
+      )
+  }
+}
+
+export default App
---
+++ src/WorldMap.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './App.css'
+import worlddata from './worlddata.js'
+import { geoMercator, geoPath } from 'd3-geo'
+
+class WorldMap extends Component {
+  render() {
+    const projection = geoMercator()
+      .scale(100)
+      .translate([430,250])
+    const pathGenerator = geoPath().projection(projection)
+      const countries = worlddata.features
+      .map((d,i) => <path
+        key={"path" + i}
+          d={pathGenerator(d)}
+          style={{ fill: this.props.colorScale(d.launchday), stroke: "black", strokeOpacity: 0.5 }}
+        className="countries"
+      />)
+      return <svg width={this.props.size[0]} height={this.props.size[1]}>
+      {countries}
+    </svg>
+  }
+}
+
+export default WorldMap
